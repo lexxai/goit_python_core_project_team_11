37eb5fb48756f4ea83ece402166e9977d3575eff
lexxai <lexxai@users.noreply.github.com>
2023-08-14 04:58:15 +0300

    test_birthday_data_format_wrong_01

--- tests/assistant_bot_test.py
+++ tests/assistant_bot_test.py
@@ -9,13 +9,30 @@ except ImportError :
     from assistant_bot.class_fields import Birthday
 
 
-class Test_Assistant_bot(unittest.TestCase):
-    def test_birthday_data_format(self):
+class Test_Assistant_bot_BirthDay(unittest.TestCase):
+    def test_birthday_data_format_EU(self):
         testcase = "12.03.2023"
         expected = "2023-03-12"
         self.assertEqual(
             str(Birthday(testcase)), expected
         )
+
+
+    def test_birthday_data_format_ISO(self):
+        testcase = "2023-03-12"
+        expected = "2023-03-12"
+        self.assertEqual(
+            str(Birthday(testcase)), expected
+        )     
+
+
+    def test_birthday_data_format_wrong_01(self):
+        testcase = "2023-33-12"
+        with self.assertRaises(ValueError) as cm:
+            Birthday(testcase)
+        the_exception = cm.exception
+        self.assertNotEqual(the_exception, "ValueError('month must be in 1..12')")
+
 
 
 unittest.main()
